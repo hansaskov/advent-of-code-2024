181e8ebd0f213ff15e5962449592e154ebf891b4
Hans Askov <hansaskov1999@gmail.com>
2024-12-03 13:13:53 +0100

    Solve day 2

--- src/solutions/day02.rs
+++ src/solutions/day02.rs
@@ -1,17 +1,44 @@
 use std::{collections::HashMap, fs};
 
 pub fn read_input() -> String {
-    fs::read_to_string("src/inputs/day01.txt").expect("Should have been able to read the file")
+    fs::read_to_string("src/inputs/day02.txt").expect("Should have been able to read the file")
 }
 
 pub fn part1(input: &str) -> i64 {
-    
-    todo!();
+    input
+        .lines()
+        .filter_map(|line| {
+            let report: Vec<_> = line
+                .split_whitespace()
+                .map(|v| v.parse::<i64>().unwrap())
+                .collect();
+
+            let diff_report: Vec<_> = report
+                .windows(2)
+                .map(|levels| levels[0] - levels[1])
+                .collect();
+
+            let is_all_ascending = diff_report.iter().all(|v| v.is_positive());
+            let is_all_decending = diff_report.iter().all(|v| v.is_negative());
+            let is_more_than_0 = diff_report.iter().all(|v| v.abs() >= 1);
+            let is_less_than_4 = diff_report.iter().all(|v| v.abs() <= 3);
+
+            if (is_all_ascending || is_all_decending) && is_more_than_0 && is_less_than_4 {
+                
+                println!("{report:#?}");
+                println!("{diff_report:#?}");
+                println!("-------------");
+                
+                return Some(1)
+            }
+            
+            return None;
+        })
+        .sum()
 }
 
 pub fn part2(input: &str) -> i64 {
-
- todo!();  
+    todo!();
 }
 
 #[cfg(test)]
@@ -26,16 +53,19 @@ mod tests {
                                  1 3 6 7 9";
 
     #[test]
-    fn test_part1_example() {
+    fn day02_part1_example() {
         assert_eq!(part1(EXAMPLE_INPUT), 2);
     }
 
+    
+
     #[test]
-    fn test_part1_file() {
+    fn day02_part1_file() {
         let input = read_input();
-        assert_eq!(part1(&input), 80085);
+        assert_eq!(part1(&input), 390);
     }
 
+    /*
     #[test]
     fn test_part2_example() {
         assert_eq!(part2(EXAMPLE_INPUT), 80085);
@@ -46,4 +76,5 @@ mod tests {
         let input = read_input();
         assert_eq!(part2(&input), 80085);
     }
+    */
 }
